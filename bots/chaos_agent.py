9446d5f3c806655d5794c1b37bc790727e50e344
Yohannes Wondimu <85318399+YWondimu@users.noreply.github.com>
2025-11-02 16:34:26 -0400

    Implement ChaosAgent with random move logic

---
+++ bots/chaos_agent.py
@@ -0,0 +1,25 @@
+from bots.base import BaseBot, Move
+import random
+
+class ChaosAgent(BaseBot):
+    counter = 0
+
+    def get_move(self, game_state):
+        rounds = self.counter // 500
+        limit = (rounds + 1)*50
+        self.counter += 1
+        rand = random.randint(1,100)
+
+        if rand > 95 and self.counter < limit:
+            result = Move.DEFECT
+        else:
+            result = Move.COOPERATE
+
+        return result
+
+        # if not game_state.opponent_history: # First round, no history yet
+        #     return Move.COOPERATE
+        # else:
+        #     opponent_last_move = game_state.last_opponent_move()
+        #     #......#
+        # return Move.COOPERATE
