4bf71df5ad34afe96b0b86360a7730a070bc19a6
khanh-1705 <140408928+khanh-1705@users.noreply.github.com>
2025-11-02 16:24:50 -0400

    Add files via upload

--- bots/mia.py
+++ bots/mia.py
@@ -1,1 +1,29 @@
+from bots.base import BaseBot, Move
+import random
 
+class Mia(BaseBot):
+    def __init__(self):
+        super().__init__()
+        self.count = 0
+        #self.forgiveness_rate = 0.2
+        self.coop_rate = 0.9
+        self.defect_rate = 0.2333
+
+    def get_move(self, game_state):
+        self.count += 1
+
+        if not game_state.opponent_history: # First round, no history yet
+            return Move.COOPERATE
+        else:
+            opponent_last_move = game_state.last_opponent_move()
+
+            if (opponent_last_move == Move.COOPERATE):
+                if random.random() < self.coop_rate:
+                    return Move.DEFECT
+                else:
+                    return Move.COOPERATE
+            else:
+                if random.random() < self.defect_rate:
+                    return Move.DEFECT
+                else:
+                    return Move.COOPERATE
